4b449cbbb9ae12bbada2754666631df457599dc8
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-14 14:26:23 +0200

    Update add_product.py

--- handlers/add_product.py
+++ handlers/add_product.py
@@ -1,37 +1,49 @@
 from telegram import Update
-from telegram.ext import ContextTypes, ConversationHandler
+from telegram.ext import ContextTypes, ConversationHandler, MessageHandler, CommandHandler, filters
 
 from database.temp_storage import PRODUCTS
 
 PHOTO, NAME = range(2)
 
-
 async def add_product_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
+    context.user_data["photos"] = []
+
     await update.message.reply_text(
-        "Отправь фото товара 🍰"
+        "Отправляй фото товара 🍰\n\nКогда закончишь — напиши: ГОТОВО"
     )
 
     return PHOTO
 
-
 async def add_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    photo = update.message.photo[-1].file_id
+    text = update.message.text
 
-    context.user_data["photo"] = photo
+    # если админ закончил
+    if text and text.upper() == "ГОТОВО":
+        await update.message.reply_text(
+            "Теперь напиши название товара:"
+        )
+        return NAME
 
-    await update.message.reply_text(
-        "Теперь напиши название товара:"
-    )
+    # если фото
+    if update.message.photo:
 
-    return NAME
+        photo = update.message.photo[-1].file_id
 
+        context.user_data["photos"].append(photo)
+
+        await update.message.reply_text(
+            f"Фото добавлено ✅ ({len(context.user_data['photos'])})\n"
+            "Можешь отправить ещё или написать ГОТОВО"
+        )
+
+    return PHOTO
 
 async def add_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     name = update.message.text
-    photo = context.user_data.get("photo")
+    photos = context.user_data.get("photos", [])
 
     if "cakes" not in PRODUCTS:
         PRODUCTS["cakes"] = []
@@ -40,11 +52,11 @@ async def add_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "name": name,
         "price": "—",
         "desc": "Добавлено через бота",
-        "photo": photo
+        "photos": photos
     })
 
     await update.message.reply_text(
-        f"✅ Товар добавлен:\n{name}"
+        f"✅ Товар добавлен:\n{name}\nФото: {len(photos)} шт."
     )
 
     return ConversationHandler.END
