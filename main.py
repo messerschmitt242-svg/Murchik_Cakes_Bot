63c18905e03f7d73fc5a484eca73882f976f9d6e
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-14 13:48:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,97 @@
 from telegram.ext import (
     Application,
-    CommandHandler
+    CommandHandler,
+    MessageHandler,
+    ConversationHandler,
+    filters
 )
 
 from config import BOT_TOKEN
 from handlers.start import start
 from handlers.id import get_id
+from handlers.catalog import catalog
+from handlers.faq import faq
 
-app = Application.builder().token(BOT_TOKEN).build()
+from handlers.order import (
+    order_start,
+    get_name,
+    get_phone,
+    get_cake,
+    NAME,
+    PHONE,
+    CAKE
+)
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("id", get_id))
+app = Application.builder().token(
+    BOT_TOKEN
+).build()
+
+app.add_handler(
+    CommandHandler(
+        "start",
+        start
+    )
+)
+
+app.add_handler(
+    CommandHandler(
+        "id",
+        get_id
+    )
+)
+
+app.add_handler(
+    MessageHandler(
+        filters.Regex("🍰 Каталог"),
+        catalog
+    )
+)
+
+app.add_handler(
+    MessageHandler(
+        filters.Regex("❓ FAQ"),
+        faq
+    )
+)
+
+order_handler = ConversationHandler(
+    entry_points=[
+        MessageHandler(
+            filters.Regex("🎂 Замовити"),
+            order_start
+        )
+    ],
+
+    states={
+
+        NAME:[
+            MessageHandler(
+                filters.TEXT,
+                get_name
+            )
+        ],
+
+        PHONE:[
+            MessageHandler(
+                filters.TEXT,
+                get_phone
+            )
+        ],
+
+        CAKE:[
+            MessageHandler(
+                filters.TEXT,
+                get_cake
+            )
+        ]
+    },
+
+    fallbacks=[]
+)
+
+app.add_handler(
+    order_handler
+)
 
 print("Bot started")
 
