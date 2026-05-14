629524a0978ce0288c3fb8dc273b9f2e58280c59
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-14 13:26:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-
-if ADMIN_ID:
-    ADMIN_ID = int(ADMIN_ID)
-else:
-    ADMIN_ID = None
+# ADMIN_ID = int(os.getenv("ADMIN_ID"))
