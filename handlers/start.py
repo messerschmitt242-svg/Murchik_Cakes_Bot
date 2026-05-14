cc44b6de3a17d9b736ec47bc349353968f6b2719
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-14 14:52:36 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -7,6 +7,6 @@ from keyboards.main_menu import main_menu
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(
-        "Добро пожаловать в кондитерскую 🎂",
+        "Вітаємо вас у кондитерскій "У Мурчика" 🎂",
         reply_markup=main_menu
     )
