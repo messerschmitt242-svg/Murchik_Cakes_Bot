9ab0747d9af13d3252bbedcc5689f66e80134caa
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-14 17:57:47 +0200

    Update products.py

--- handlers/products.py
+++ handlers/products.py
@@ -1,57 +1,75 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+
 from telegram.ext import ContextTypes
 
 from database.products_db import get_all_products
-from keyboards.main_menu import main_menu
+
+import json
 
 
-async def show_products(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def show_products(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
 
     products = get_all_products()
 
     if not products:
+
         await update.message.reply_text(
-            "Каталог пустий 🍰",
-            reply_markup=main_menu
+            "Каталог порожній 🍰"
         )
+
         return
 
     for p in products:
 
-        product_id = p.get("id")
-        name = p.get("name", "Без названия")
-        price = p.get("price", "—")
-        desc = p.get("desc", "—")
-        photos = p.get("photos", [])
+        try:
+            photos = json.loads(
+                p["photos"]
+            )
+
+        except:
+            photos = []
 
         caption = f"""
-🍰 {name}
-💰 {price}
-📝 {desc}
+🍰 {p['name']}
+
+💰 {p['price']} zł
+
+📝 {p['description']}
 """
 
         keyboard = InlineKeyboardMarkup([
             [
                 InlineKeyboardButton(
-                    "🛒 Додати в кошик",
-                    callback_data=f"add_{product_id}"
+                    "🛒 Додати у кошик",
+                    callback_data=f"add_{p['id']}"
                 )
             ]
         ])
 
         if photos:
+
             await update.message.reply_photo(
                 photo=photos[0],
                 caption=caption,
                 reply_markup=keyboard
             )
+
+            for ph in photos[1:]:
+
+                await update.message.reply_photo(
+                    photo=ph
+                )
+
         else:
+
             await update.message.reply_text(
                 caption,
                 reply_markup=keyboard
             )
-
-    await update.message.reply_text(
-        "📍 Оберіть товар або відкрийте кошик 🛒",
-        reply_markup=main_menu
-    )
