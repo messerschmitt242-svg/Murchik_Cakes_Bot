172f5e9d0c03dac1e3b6e981c02af204dc842145
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-14 14:02:06 +0200

    Add files via upload

---
+++ keyboards/catalog_menu.py
@@ -0,0 +1,10 @@
+from telegram import ReplyKeyboardMarkup
+
+catalog_menu = ReplyKeyboardMarkup(
+    [
+        ["🍰 Торти"],
+        ["🍮 Тістечка"],
+        ["⬅️ Назад"]
+    ],
+    resize_keyboard=True
+)
