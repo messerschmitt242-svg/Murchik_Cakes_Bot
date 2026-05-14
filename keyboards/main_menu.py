113b459951048ef277a88e2ca647af26d86cc279
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-14 16:00:16 +0200

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -3,6 +3,7 @@ from telegram import ReplyKeyboardMarkup
 main_menu = ReplyKeyboardMarkup(
     [
         ["🍰 Каталог", "🎂 Замовити"],
+        ["📦 Мої замовлення"],
         ["❓ FAQ", "📍 Контакти"]
     ],
     resize_keyboard=True
