52dc5e207626830ed5fbbaa3766145684d284c4c
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-14 15:49:39 +0200

    Create contacts.py

---
+++ handlers/contacts.py
@@ -0,0 +1,38 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes
+
+
+async def contacts(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    text = """
+📍 Наші контакти
+
+📞 Номер телефону:
++48 504 690 652
+
+🕒 Графік роботи:
+Пн–Нд: 10:00 – 18:00
+
+🏠 Адреса:
+ul. Toruńska 45D, Bydgoszcz
+"""
+
+    keyboard = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton(
+                "📍 Побудувати маршрут",
+                url="https://maps.app.goo.gl/h3XoRor8GWZucXFj8"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "📞 Зателефонувати",
+                url="tel:+48504690652"
+            )
+        ]
+    ])
+
+    await update.message.reply_text(
+        text,
+        reply_markup=keyboard
+    )
